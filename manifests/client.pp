48ed083ade06eccf94a7f7028787dfab890a0125
Greg Sutcliffe <gsutclif@redhat.com>
2013-07-02 16:54:49 +0100

    Initial commit

---
+++ manifests/client.pp
@@ -0,0 +1,43 @@
+# Trivial class to include the dirvish keys on the clients
+#
+# Requires that /root/.ssh/authorized_keys is present
+#
+class dirvish::client (
+  $pre_script    = 'undef',
+  $pre_template  = 'undef',
+  $declare_rsync = true
+) {
+
+  # Read the dirvish key from the puppetmaster
+  $pub_key  = ssh_keygen({name => 'dirvish_key', public => 'true'})
+
+  file_line { 'dirvish_ssh_pubkey':
+    ensure => present,
+    path   => '/root/.ssh/authorized_keys',
+    line   => "ssh-rsa ${pub_key} dirvish_key\n",
+  }
+
+  $template_content = $pre_template ? {
+    'undef' => template('dirvish/pre_client.sh.erb'),
+    default => template($pre_template),
+  }
+
+  $content = $pre_script ? {
+    'undef' => $template_content,
+    default => $pre_script,
+  }
+
+  # Basic pre-run script
+  file { '/etc/dirvish/pre_client':
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0755,
+    content => $content,
+  }
+
+  # Dirvish depends on rsync
+  if $declare_rsync {
+    package { 'rsync': ensure => installed }
+  }
+
+}
